96e3bd7aa797ce7129b0bcefe9230287d7a3266f
Paulo Keller <paulokeller84@gmail.com>
2022-02-16 09:48:59 -0300

    Adicionar arquivos de projeto.

---
+++ questao01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace questao01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, lin, col, qtd;
+
+            Console.WriteLine("Digite a quantidade de degraus\n");
+            qtd = Convert.ToInt32(Console.ReadLine());
+            n = 1;
+
+            for (lin = 0; lin < qtd; lin++)
+            {
+                for (col = 0; col < n; col++)
+                {
+                    Console.Write("*");
+                }
+
+                n = n + 1;
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
